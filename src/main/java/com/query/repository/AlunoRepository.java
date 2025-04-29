ed7f74560cd6aa92d8710b610913d4116e25fcec
Rudrian <rudrian321@gmail.com>
2025-04-29 09:46:34 -0300

    QueryMETHODS

---
+++ src/main/java/com/query/repository/AlunoRepository.java
@@ -0,0 +1,27 @@
+package com.query.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.query.entities.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> { 
+	List<Aluno> findByCidade(String cidade);
+	
+	List<Aluno> findByNome(String nome);
+	
+	List<Aluno> findByEmail(String email);
+	
+	List<Aluno> findByRenda(double renda);
+	
+	List<Aluno> findByRa(String ra);
+	//Query Methods
+	
+}
+
+	
+	
+	
+	
+	
+	
